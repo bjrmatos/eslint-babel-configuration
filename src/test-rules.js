2f9a7a944d81d6a74e3bf5195bbe0d8f03928db4
BJR Matos <bjrmatos@gmail.com>
2015-09-08 11:52:47 -0500

    [update] reglas de eslint (ES6, ES5)

---
+++ src/test-rules.js
@@ -0,0 +1,27 @@
+
+let x,
+    y,
+    z;
+
+var a,
+    b,
+    c;
+
+const df = 2,
+      x = 6;
+
+if (true) {
+
+}
+
+if (!!x) {
+
+}
+
+if (x === 2) { console.log(3); };
+
+if (x == null) {
+    x = 2;
+}
+
+console.log('it works!!');
